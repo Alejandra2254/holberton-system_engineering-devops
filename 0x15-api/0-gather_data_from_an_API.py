e905967bf089c8020f3eb5fe446957e1a849f437
Alejandra2254 <2254@holbertonschool.com>
2021-05-03 15:42:21 -0500

    Fixing output

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Using this REST API, function accept an integer as paramete
 Return: display on the standard output the employee TODO list progress"""
-import requests
-import sys
-
 
 if __name__ == "__main__":
+    import requests
+    import sys
+    
     id_employee = sys.argv[1]
     general = requests.get('https://jsonplaceholder.typicode.com/users/' +
                            id_employee)
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     for task in tasks_employee:
         if task['completed'] is True:
             completed.append(task)
-    print('Employee {} is done with tasks ({}/{}):'
+    print('Employee {} is done with tasks({}/{}):'
           .format(name_employee, len(completed), len(tasks_employee)))
     for title in completed:
         print("\t" + title['title'], end="\n")
