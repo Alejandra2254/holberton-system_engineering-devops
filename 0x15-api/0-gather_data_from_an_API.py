f81bd2771499b2fb1f4a8cd21794209cf058f9b2
Alejandra2254 <2254@holbertonschool.com>
2021-05-03 11:35:56 -0500

    Fixing pep8 style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
+"""Using this REST API, function accept an integer as paramete
+Return: display on the standard output the employee TODO list progress"""
 import requests
 import sys
 
+
 if __name__ == "__main__":
     id_employee = sys.argv[1]
-    general = requests.get('https://jsonplaceholder.typicode.com/users/' + id_employee)
-    tasks = requests.get('https://jsonplaceholder.typicode.com/users/' + id_employee + '/todos')
+    general = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                           id_employee)
+    tasks = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                         id_employee + '/todos')
     name_employee = general.json()['name']
     tasks_employee = tasks.json()
     completed = []
     for task in tasks_employee:
         if task['completed'] is True:
             completed.append(task)
-    print('Employee {} is done with tasks ({}/{}):'.format(name_employee, len(completed), len(tasks_employee)))
+    print('Employee {} is done with tasks ({}/{}):'
+          .format(name_employee, len(completed), len(tasks_employee)))
     for title in completed:
         print("\t" + title['title'], end="\n")
-
-
